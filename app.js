ac0f9d391ac3613d4af461831ff1c5d40f0d360d
SEFSIS <sofiagryn@icloud.com>
2023-07-11 17:48:49 +0300

    hw2

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
+const express = require('express');
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.get('/users/:id', (req, res) => {
+    const {id} = req.params;
+
+    res.status(200).json(users[+id]);
+})
+
+app.post('/users',(req,res)=>{
+    users.push(req.body);
+
+    res.status(201).json({
+        message:"User created."
+    });
+})
 
 
 
