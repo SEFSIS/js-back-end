d4ec50cfa835f089fc5c67d51bd2df2cd953b992
SEFSIS <sofiagryn@icloud.com>
2023-08-02 23:26:21 +0300

    lesson5

---
+++ dist/repositories/user.repository.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userRepository = void 0;
+const User_1 = require("../models/User");
+class UserRepository {
+    async create(data) {
+        return User_1.User.create(data);
+    }
+}
+exports.userRepository = new UserRepository();
