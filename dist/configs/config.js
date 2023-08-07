284704c2e7fe8b4faff317de7cb7b0d775d0ce95
SEFSIS <sofiagryn@icloud.com>
2023-08-07 23:26:17 +0300

    lesson7

--- dist/configs/config.js
+++ dist/configs/config.js
@@ -9,4 +9,6 @@ exports.configs = {
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
     SECRET_SALT: process.env.SECRET_SALT,
+    NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL,
+    NO_REPLY_PASSWORD: process.env.NO_REPLY_PASSWORD,
 };
--- dist/services/auth.service.js
+++ dist/services/auth.service.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.authService = void 0;
+const email_enum_1 = require("../enums/email.enum");
 const errors_1 = require("../errors");
 const Token_model_1 = require("../models/Token.model");
 const User_1 = require("../models/User");
+const email_service_1 = require("./email.service");
 const password_service_1 = require("./password.service");
 const token_service_1 = require("./token.service");
 class AuthService {
@@ -11,6 +13,9 @@ class AuthService {
         try {
             const hashedPassword = await password_service_1.passwordService.hash(data.password);
             await User_1.User.create({ ...data, password: hashedPassword });
+            await email_service_1.emailService.sendMail(data.email, email_enum_1.EEmailActions.WELCOME, {
+                name: data.name,
+            });
         }
         catch (e) {
             throw new errors_1.ApiError(e.message, e.status);
